ffdb21fb3b488821ca06f6a065a211066c398f8d
Tyler Robbins <tyler@familyrobbins.com>
2016-04-07 22:52:05 -0700

    Made EventBase a struct. Removed everything that made it a class.

--- EventRobot/includes/events/EventBase.h
+++ EventRobot/includes/events/EventBase.h
@@ -1,23 +1,12 @@
 #ifndef _EVENT_BASE_H_
 #define _EVENT_BASE_H_
 
-class EventBus;
-
-class EventBase {
-    public:
-        EventBase();
-        ~EventBase();
-        inline bool isCanceled(){ return m_canceled; }
-        inline void setCanceled(bool value){ m_canceled = value; }
-
+typedef struct struct_EventBase {
         int eid;
         int lifecycle;
 
-    private:
         bool m_canceled;
-
-    friend class EventBus;
-};
+} EventBase;
 
 #endif
 
