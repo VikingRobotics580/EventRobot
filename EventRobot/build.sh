4d7074e10f4a2216a303de114f47e986e214d60c
Tyler Robbins <tyler@familyrobbins.com>
2016-04-19 13:17:33 -0700

    Fixed the compile thing. Added command which generates the .a file.

--- EventRobot/build.sh
+++ EventRobot/build.sh
@@ -2,6 +2,8 @@
 
 for f in ./src/*.cpp
 do
-    arm-frc-linux-gnueabi-g++ -g -Wall -W -I`echo ~`/wpilib/cpp/current/include -I. -std=c++14 -fPIC -L`echo ~`/wpilib/cpp/current/lib -lwpi "$f" -o $(basename "$f").o
+    arm-frc-linux-gnueabi-g++ -g -c -Wall -W -I`echo ~`/wpilib/cpp/current/include -I. -std=c++14 "$f" -o $(basename "$f").o || { exit 1; }
 done
+
+ar rvs eventrobot.a ./*.o || { exit 1; }
 
