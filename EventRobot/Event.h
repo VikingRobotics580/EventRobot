d478c987bae6aebe431d57a9998463288780503b
Tyler Robbins <tyler@familyrobbins.com>
2016-04-19 14:14:15 -0700

    Added a simple include file to get everything in the project.

---
+++ EventRobot/Event.h
@@ -0,0 +1,14 @@
+#ifndef _EVENT_H_
+#define _EVENT_H_
+
+#include "includes/EventBus.h"
+#include "includes/EventRobot.h"
+#include "includes/event_robot_types.h"
+#include "includes/events/EventBase.h"
+#include "includes/events/ModeChangeEvent.h"
+#include "includes/events/TickEvent.h"
+#include "includes/handlers/EventHandler.h"
+#include "includes/handlers/RobotHandler.h"
+
+#endif
+
