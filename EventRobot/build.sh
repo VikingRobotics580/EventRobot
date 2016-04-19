1651287d124a2f0372440201b0c19f7c1e06df1b
Tyler Robbins <tyler@familyrobbins.com>
2016-04-19 13:39:18 -0700

    Fixed some more stuff in build script.

--- EventRobot/build.sh
+++ EventRobot/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for f in ./src/*.cpp
+for f in $(find src -name "*.cpp")
 do
+    echo "Compiling $f"
     arm-frc-linux-gnueabi-g++ -g -c -Wall -W -I`echo ~`/wpilib/cpp/current/include -I. -std=c++14 "$f" -o $(basename "$f").o || { exit 1; }
 done
 
-ar rvs eventrobot.a ./*.o || { exit 1; }
+ar rvs libs/libeventrobot.a ./*.o || { exit 1; }
 
