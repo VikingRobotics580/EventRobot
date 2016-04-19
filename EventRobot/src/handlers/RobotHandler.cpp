a7a9308e1a6c07c3bf4346a895d63a3dddccd2f9
Tyler Robbins <tyler@familyrobbins.com>
2016-04-19 14:13:51 -0700

    Fixed errors that occurred when including files. Fixed a compile issue that occurred due to the wrong accessor operator being used. Fixed a compile error that occurred due to the wrong member variable being used.

--- EventRobot/src/handlers/RobotHandler.cpp
+++ EventRobot/src/handlers/RobotHandler.cpp
@@ -1,9 +1,7 @@
-#include "RobotHandler.h"
-#include "events/TickEvent.h"
-#include "events/PreModeChangeEvent.h"
-#include "events/ModeChangeEvent.h"
-#include "events/PostModeChangeEvent.h"
-#include "EventRobot.h"
+#include "includes/handlers/RobotHandler.h"
+#include "includes/events/TickEvent.h"
+#include "includes/events/ModeChangeEvent.h"
+#include "includes/EventRobot.h"
 
 RobotHandler::RobotHandler():
     EventHandler()
@@ -11,9 +9,9 @@ RobotHandler::RobotHandler():
 
 RobotHandler::~RobotHandler(){ }
 
-int RobotHandler::Update(event_list events){
+int RobotHandler::Update(event_list& events){
     for(auto& event : events){
-        if(event.eid == TickEvent::ID){
+        if(event->eid == TickEvent::ID){
             EventRobot* caller = ((TickEvent*)event)->caller;
 
             Modes mode = caller->getMode();
@@ -28,12 +26,13 @@ int RobotHandler::Update(event_list events){
         // PreModeChange -> ModeChange -> PostModeChange
         // We catch each one and make sure they are called one after the other
         // Make sure to check the lifecycle so that we only call it after it has made the rounds
-        }else if(event.eid == PreModeChangeEvent::ID && event.lifecycle >= 1){
-            event->setCanceled(true);
-            EventRobot::EVENT_BUS->post(new ModeChangeEvent(mode,newMode));
-        }else if(event.eid == ModeChangeEvent::ID && event.lifecycle >= 1){
-            event->setCanceled(true);
-            EventRobot::EVENT_BUS->post(new PostModeChangeEvent(mode,newMode));
+        }else if(event->eid == PreModeChangeEvent::ID && event->lifecycle >= 1){
+            event->canceled = true;
+            EventRobot::EVENT_BUS->post(new ModeChangeEvent(((ModeChangeEvent*)event)->lastMode,((ModeChangeEvent*)event)->nextMode));
+        }else if(event->eid == ModeChangeEvent::ID && event->lifecycle >= 1){
+            event->canceled = true;
+            //EventRobot::EVENT_BUS->post(new PostModeChangeEvent(mode,newMode));
+            EventRobot::EVENT_BUS->post(new PostModeChangeEvent(((ModeChangeEvent*)event)->lastMode,((ModeChangeEvent*)event)->nextMode));
         }
     }
     return 0;
