322b6ce564fe6b49c115893c4bd34df65ac6dde3
Tyler Robbins <tyler@familyrobbins.com>
2016-05-27 15:12:29 -0700

    Modified EventBase so that it uses the Curiously Reccurring Template Pattern (CRTP) idiom to get its real type. Added an interface that it extends.

--- EventRobot/includes/events/EventBase.h
+++ EventRobot/includes/events/EventBase.h
@@ -1,11 +1,18 @@
 #ifndef _EVENT_BASE_H_
 #define _EVENT_BASE_H_
 
-typedef struct struct_EventBase {
+struct IEventBase{};
+
+template<typename T>
+struct EventBase: public IEventBase {
         int eid;
         int lifecycle;
         bool canceled;
-} EventBase;
+
+        inline T* getActualType(){
+            return static_cast<T*>(this);
+        }
+};
 
 #endif
 
