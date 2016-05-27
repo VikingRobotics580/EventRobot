814d0df6653119cfdec98859016e1d010b46110a
Tyler Robbins <tyler@familyrobbins.com>
2016-05-27 15:08:56 -0700

    Rewrote EVENT_SUBSCRIBE so that it actually works .

--- EventRobot/includes/EventSubscribe.h
+++ EventRobot/includes/EventSubscribe.h
@@ -4,18 +4,10 @@
 #include "event_robot_types.h"
 
 // A preprocessor macro which defines a special method to be called by EventRobot when a handler is doing its job
-#define EVENT_SUBSCRIBE(EVENTTYPE,PRIORITY,func) \
-    public: \
-        template<> \
-        void __handle_event<EVENTTYPE>(EVENTTYPE e){ \
-            /* Ensure that the function is registered*/ \
-            if(this->__func_idx__ == NULL){ \
-                std::function<void(EVENTTYPE)>* f = func; \
-                /*this->_functions.at(PRIORITY).push_back(f); */\
-            } \
-            func(e); \
-
-#define EventSubscribe(e,f) EVENT_SUBSCRIBE(e,Priorities::NORMAL,f)
+#define EVENT_SUBSCRIBE(EVENTTYPE,PRIORITY,FUNCTION) \
+        void __handle_event_##PRIORITY (EVENTTYPE* e){ \
+            FUNCTION(e); \
+        }
 
 #endif
 
