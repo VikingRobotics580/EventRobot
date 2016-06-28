d0ab8638651470c6259b531d974fd266ab4d19bf
Tyler Robbins <tyler@familyrobbins.com>
2016-06-28 09:09:54 -0700

    Added a new way to subscribe to events that should work better.

--- EventRobot/includes/EventSubscribe.h
+++ EventRobot/includes/EventSubscribe.h
@@ -2,12 +2,23 @@
 #define _EVENT_SUBSCRIBE_H_
 
 #include "event_robot_types.h"
+#include "events/EventBase.h"
 
 // A preprocessor macro which defines a special method to be called by EventRobot when a handler is doing its job
 #define EVENT_SUBSCRIBE(EVENTTYPE,PRIORITY,FUNCTION) \
         void __handle_event_##PRIORITY (EVENTTYPE* e){ \
             FUNCTION(e); \
         }
+
+#define SUBSCRIBE_START \
+    void subscribe(IEventBase* _event){
+#define SUBSCRIBE_END \
+        return; \
+    }
+#define SUBSCRIBE(TYPE,PRIORITY,FUNC) \
+    if(reinterpret_cast<TYPE*>(_event) && _event->priority == PRIORITY){ \
+        FUNC(reinterpret_cast<TYPE*>(_event)); \
+    }else 
 
 #endif
 
