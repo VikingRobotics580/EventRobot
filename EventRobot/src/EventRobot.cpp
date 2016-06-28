7d19754c653bd1ac52edd626a7de8d1b7f8d29e1
Tyler Robbins <tyler@familyrobbins.com>
2016-06-28 09:13:42 -0700

    Commented out old __handle_event method, and add some code which calls the new method.

--- EventRobot/src/EventRobot.cpp
+++ EventRobot/src/EventRobot.cpp
@@ -50,11 +50,14 @@ void EventRobot::StartCompetition(){
         EVENT_BUS->post(new TickEvent(this));
         for(auto& event : EVENT_BUS->getEvents()){
             for(auto& handler : m_handlers){
+                handler->subscribe(event);
+                /*
                 handler->__handle_event_HIGHEST(event->getActualType());
                 handler->__handle_event_HIGH(event->getActualType());
                 handler->__handle_event_NORMAL(event->getActualType());
                 handler->__handle_event_LOW(event->getActualType());
                 handler->__handle_event_LOWEST(event->getActualType());
+                */
             }
         }
         EVENT_BUS->Update();
